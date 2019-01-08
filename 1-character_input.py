2853091d33475cfccd07eb88760ebf0c76228b4c
Rose <rose.xw.wan@gmai.com>
2019-01-08 14:00:51 -0500

    Added extras to first task

--- 1-character_input.py
+++ 1-character_input.py
@@ -1,4 +1,21 @@
-name = raw_input("What is your name: ")
-age = int(input("How old are you: "))
-year = str((2018 - age)+100)
-print(name + " will be 100 years old in the year " + year)
+# Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+# name = raw_input("What is your name: ")
+# age = int(input("How old are you: "))
+# year = str((2018 - age)+100)
+# print(name + " will be 100 years old in the year " + year)
+
+
+# print("Were" + "wolf")
+# print("Door" + "man")
+# print("4" + "chan")
+# print(str(4) + "chan")
+
+# Extras:
+
+# Add on to the previous program by asking the user for another number and printing out that many copies of the previous message. (Hint: order of operations exists in Python)
+print(3 * "test")
+
+# Print out that many copies of the previous message on separate lines. (Hint: the string "\n is the same as pressing the ENTER button)
+
+print(3 * "test\n")
