5b6e0c8f0e86856a26d60d5457ce547e7184ff1c
Rose <rose.xw.wan@gmai.com>
2019-01-14 17:17:20 -0500

    Added some splicing to the palindrome exercise

--- 6-StringLists.py
+++ 6-StringLists.py
@@ -4,11 +4,24 @@
 # Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
 
 word = str(raw_input("Please enter the word you would like to test if a palindrome: "))
+
 # Negative values also work to make a copy of the same list in reverse order:
 # >>> L[::-1]
+
 rvs=word[::-1]
 print("Reverse of word is: " + rvs)
 if word == rvs:
     print("This word is a palindrome")
 else:
     print("This word is not a palindrome")
+
+
+# # Slicing a word by even numbers:
+# L = range(10)
+# print L[::2]
+
+# # by factors of three
+# L = range(10)
+# print L[::3]
+
+# https://docs.python.org/2.3/whatsnew/section-slices.html
