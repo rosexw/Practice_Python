2d950fe3e2390a3bbb0df0606206f11659e25df0
Rose <rose@cluep.com>
2019-02-04 11:43:51 -0500

    completed 17-decode-a-webpage with BeautifulSoup

--- 17-decode-a-webpage.py
+++ 17-decode-a-webpage.py
@@ -1,57 +1,13 @@
 # Exercise 17 - Decode A Web Page 
 # Use the BeautifulSoup and requests Python packages to print out a list of all the article titles on the New York Times homepage.
 
-# import requests
-# from bs4 import BeautifulSoup
-
-# url = 'https://www.nytimes.com/'
-# # url = 'http://github.com'
-# r = requests.get(url)
-# r_html = r.text
-
-# soup = BeautifulSoup(r_html)
-# title = soup.find('span', 'balancedHeadline').string
-
-# print(title)
-
 import requests
 from bs4 import BeautifulSoup
 
-# 1st way
-# url = 'http://www.nytimes.com'
-# r = requests.get(url)
-# r_html = r.text
-# soup = BeautifulSoup(r_html, "lxml")
-# title = soup.findAll(class_="balancedHeadline")
+url = 'https://www.nytimes.com/'
+r = requests.get(url)
+nyt_html = r.text
 
-# 2nd way
-# r = requests.get("https://www.nytimes.com")
-# soup = BeautifulSoup(r.content, "html.parser")
-# titles = soup.find_all('span', {'class': 'balancedHeadline'})
-
-# title.extend(soup.find_all("h1"))
-# title.extend(soup.find_all("h2"))
-# title.extend(soup.find_all("h3"))
-# title.extend(soup.find_all("h4"))
-# title.extend(soup.find_all("h5"))
-# title.extend(soup.find_all("h6"))
-
-# print([x.get_text() for x in title])
-
-# for item in title:
-#     try:
-#         print item.text.strip()
-#     except:
-#         pass
-
-
-# 3rd way
-base_url = 'http://www.nytimes.com'
-r = requests.get(base_url)
-soup = BeautifulSoup(r.text, features="lxml")
- 
-for story_heading in soup.find_all(class_="balancedHeadline"): 
-    if story_heading: 
-        print(story_heading.text.replace("\n", " ").strip())
-    else: 
-        print(story_heading.contents[0].strip())
+soup = BeautifulSoup(nyt_html,features="html.parser")
+for h2 in soup.findAll("h2", class_="css-bzeb53 esl82me2"):
+    print(h2.text)
