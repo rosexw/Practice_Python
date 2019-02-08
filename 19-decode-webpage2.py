c15fb7ac86ec5681554db369ce081d301d1218da
Rose <rose@cluep.com>
2019-02-08 12:15:28 -0500

    added basic parsing to exercise 19

--- 19-decode-webpage2.py
+++ 19-decode-webpage2.py
@@ -3,17 +3,14 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 source = requests.get("http://content.time.com/time/magazine/article/0,9171,2005869,00.html").text
-source2 = requests.get("http://content.time.com/time/magazine/article/0,9171,2005869-2,00.html").text
 soup = BeautifulSoup(source, 'lxml')
-soup2 = BeautifulSoup(source2, 'lxml')
 
 print(soup.title.text)
+
+
 
 for i in soup.find_all('p'):
     article = i.text
     print(article)
-
-for j in soup2.find_all('p'):
-    article2 = j.text
-    print(article2)
